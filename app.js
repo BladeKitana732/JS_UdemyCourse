e50b527f9dcbc665c116f96c5cbfa4193633f1ff
teresa <teresa@teresas-Air.attlocal.net>
2022-05-26 22:14:30 -0500

    array and array methods

--- app.js
+++ app.js
@@ -1,39 +1,51 @@
-//Template literals 
-//inserting HTML through Javascript 
+//Array & methods 
+const numbers = [43, 56, 33, 23, 44 ,36, 5];
+const numbers2 = new Array(22, 45, 33, 76, 54);
+const fruit = ['apple', 'banana', 'orange', 'pear'];
+const mix = [22, 'hello', true, undefined, null, {a:1, b:1}, new Date()];
 
-const name = 'John';
-const age = 31;
-const job = 'Web Dev';
-const city = 'Miami';
-let html;
+let val; 
 
-//w/o template strings (ES5)
-html = '<ul><li>Name: '+ name + '</li><li>Age:' + age + '</li><li>Job:' + job + '</li><li>City:' + city + '</li></ul>';
+val = numbers.length;//get array length
+val = Array.isArray(numbers);//check if array
+val = numbers[3];//get single value; 0 based
 
-html = '<ul>' + 
-       '</li><li>Name: ' + name + '</li>' +
-       '</li><li>Age: ' + age + '</li>' +
-       '</li><li>Job: ' + job + '</li>'+
-       '</li><li>City: ' + city + '</li>' +
-       '</ul>';
+//insert into array 
+numbers[2] = 100;
 
-//with template strings
-function hello() {
-    return 'hello';
+//find idex of value
+val = numbers.indexOf(36);
+
+//Mutating 
+// numbers.push(250);//add to the end
+// numbers.unshift(120);//add to front
+// numbers.pop();//take off from end
+// numbers.shift();//take off from front
+// numbers.splice(1,3)//splice values (where we start, where we end)
+// numbers.reverse();//reverse
+
+//concatenate
+val = numbers.concat(numbers2);
+//sort
+val = fruit.sort();//alphabetical order
+// val = numbers.sort();//sorts by first number; fix by using "compare function"
+// val = numbers.sort(function(x,y){
+//     return x - y
+// })
+
+// //reverse sort 
+// val = numbers.sort(function(x,y){
+//     return y - x
+// });
+
+//Find 
+function under50(num) {
+    return num < 50;
 }
 
+val = numbers.find(under50);
 
-html = `
-    <ul>
-        <li>Name: ${name}</li>
-        <li>Age: ${age}</li>
-        <li>Job: ${job}</li>
-        <li>City: ${city}</li>
-        <li>${2 + 2}</li>
-        <li>${hello()}</li>
-        <li>${age > 30 ? 'Over 30' : 'Under 30'}</li> 
 
-    </ul>
-`;
 
-document.body.innerHTML = html;
+console.log(numbers);
+console.log(val);
