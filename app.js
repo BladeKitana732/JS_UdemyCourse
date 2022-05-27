cdd5c31f199ef69443e9cc96a5b939e048366a92
teresa <teresa@teresas-Air.attlocal.net>
2022-05-26 20:37:07 -0500

    type coercion and conversions explained

--- app.js
+++ app.js
@@ -1,21 +1,53 @@
-//Primitive data types: string, number, boolean, null, undefined, symbols (ES6)
+//Type conversion - taking variable and changing data type; 
 
-const name = 'John Doe';
-const age = 45;
-const hasKids = false;
-const car = null;
-let test;
-const sym = Symbol();
+let val;
 
-console.log(typeof test);
+//Number to string 
+val = String(555);
+val = String(4+4);
 
-//Reference/Objects: Arrays, Object literals, Functions, Dates, anything else...
+//Boolean  to string
+ val = String(true);
 
-const hobbies = ['movies', 'music'];
-const address = {
-    city: 'Boston',
-    state: 'MA'
-}
-const today = new Date();
+ //date to string
+ val = String(new Date());
 
-console.log(today);
+ //array to string
+ val = String([1,2,3,4]);
+
+//toString()
+val = (5).toString();
+val = (true).toString();
+
+//String to number
+val = Number('5');
+ 
+//Boolean to number
+val = Number(true);
+val = Number(false);
+val = Number(null);
+
+//NaN - not a number 
+val = Number('hello');
+val = Number([1,2,3]);
+
+//parseInt
+val = parseInt('100');
+val = parseFloat('100.30');//decimals
+
+//Output 
+// console.log(val);
+// console.log(typeof val);
+// //console.log(val.length); //length only works on strings
+// console.log(val.toFixed(2));//specify decimals. only works on numbers
+
+//string + num == string/ type coercion 
+const val1 = String(5);
+const val2 = 6;
+
+const sum = Number(val1 + val2);
+
+console.log(sum);
+console.log(typeof sum);
+
+
