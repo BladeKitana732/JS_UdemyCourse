25b753ff6122d639bd536e48acf80a6effe125b7
teresa <teresa@teresas-Air.attlocal.net>
2022-05-26 23:21:21 -0500

    swtich statement

--- app.js
+++ app.js
@@ -1,86 +1,43 @@
-//If statement and comparison operators
-// if(something) {
-//     do something
-// } else {
-//     do something else
-// }
-
-const id = 100;
-
-//Equal to == testing VALUE not the type (single = is assigning)
-// if(id == 100){
-//     console.log('correct')
-// } else {
-//     console.log('incorrect')
-// }
-
-// //not equal to 
-// if(id != 101){
-//     console.log('correct')
-// } else {
-//     console.log('incorrect')
-// }
-
-// //Equal to value & type ===
-// if(id === 100){
-//     console.log('correct')
-// } else {
-//     console.log('incorrect')
-// }
-
-// //Not Equal to value & type ===
-// if(id !== 100){
-//     console.log('correct')
-// } else {
-//     console.log('incorrect')
-// }
-
-//testing type if undefined 
-// if(typeof id !== 'undefined'){
-//     console.log(`The ID is ${id}`);
-// } else {
-//     console.log('NO ID');
-// }
-
-//greater or less 
-// if(id <= 200) {
-//     console.log('Correct');
-// } else {
-//     console.log('Incorrect');
-// }
-
-//IF Else
+// switch statement 
 
 const color = 'yellow';
 
-// if(color ==='red'){
-//     console.log('color is red');
-// } else if(color === 'blue') {
-//     console.log('color if blue')
-// } else {
-//     console.log('color is not red or blue');
-// }
-
-//Logical Operators
-
-const name = 'Steve';
-const age = 20;
-
-//AND &&
-if(age > 0 && age < 12) {
-    console.log(`${name} is a child`);
-} else if(age >= 13 && age <= 19) {
-    console.log(`${name} is a teenager`);
-} else {
-    console.log(`${name} is an adult`)
+switch(color){
+    case 'red':
+        console.log('color is red');
+        break;
+    case 'blue':
+        console.log('color is blue');
+        break;
+    default:
+        console.log('Color is not red or blue');
+        break;        
 }
 
-//OR ||
-if(age < 16 || age > 65){
-    console.log(`${name} cannot run in the race`);
-} else {
-    console.log(`${name} is registered for the race`);
+let day; 
+
+switch(new Date().getDay()) {
+    case 0:
+        day = 'Sunday';
+        break;
+    case 1:
+        day = 'Monday';
+        break; 
+    case 2:
+        day = 'Tuesday';
+        break;
+    case 3:
+        day = 'Wednesday';
+        break;
+    case 4:
+        day = 'Thursday';
+        break;
+    case 5:
+        day = 'Friday';
+        break;
+    case 6:
+        day = 'Saturday';
+        break;
 }
 
-//ternary operator 
-console.log(id === 100 ? 'correct' : 'incorrect');
+console.log(`Today is ${day}`);
