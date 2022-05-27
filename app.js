16cb9fb7938bcb063c519672e155a75e43826980
teresa <teresa@teresas-Air.attlocal.net>
2022-05-26 19:58:56 -0500

    using the console

---
+++ app.js
@@ -0,0 +1,28 @@
+// Log to console
+console.log('Hello World');
+console.log(123);
+console.log(true);
+var greeting = 'Hello';
+console.log(greeting);
+console.log([1,2,3,4]);
+console.log({a:1, b:2});
+console.table({a:1, b:2});
+
+console.error('This is some error');
+console.warn('this is a warning');
+
+console.time('Hello');
+    console.log('You timed me');
+    console.log('You timed me');
+    console.log('You timed me');
+    console.log('You timed me');
+    console.log('You timed me');
+console.timeEnd('Hello');
+
+console.clear();
+
+/*
+multi 
+line 
+comments
+*/
