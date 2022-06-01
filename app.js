b1c689c277bd63b6062b58a11d55c67c68acea93
teresa <teresa@teresas-Air.attlocal.net>
2022-06-01 15:08:55 -0500

    general loops

--- app.js
+++ app.js
@@ -1,46 +1,76 @@
-//Function (blocks of code that can be used) declarations & expressions
+//General Loops- repeats until a specific condition is met 
 
-function greet(firstName = 'John', lastName = 'Doe') { //can set default w/in parameter ES6
-    // if(typeof firstName === 'undefined'){firstName = 'John'}//default for ES5
-    // if(typeof lastName === 'undefined'){lastName = 'Doe'}//default for ES5
-    //console.log('hello');
-    return 'hello ' + firstName + ' ' + lastName;
+// FOR LOOP
+
+// for(let i = 0; i < 10; i++){
+//   if(i === 2){
+//     console.log('2 is my favorite number');
+//     continue;
+//   }
+
+//   if(i === 5){
+//     console.log('Stop the loop');
+//     break;
+//   }
+
+//   console.log('Number '+ i);
+// }
+
+// WHILE LOOP
+
+// let i = 0;
+
+// while(i < 10){
+//   console.log('Number ' + i);
+//   i++;
+// }
+
+// DO WHILE
+
+// let i = 100;
+
+// do {
+//   console.log('Number ' + i);
+//   i++;
+// }
+
+// while(i < 10);
+
+// LOOP THROUGH ARRAY
+const cars = ['Ford', 'Chevy', 'Honda', 'Toyota'];
+
+// for(let i = 0; i < cars.length; i++){
+//   console.log(cars[i]);
+// }
+
+// FOREACH
+// cars.forEach(function(car, index, array){
+//   console.log(`${index} : ${car}`);
+//   console.log(array);
+// });
+
+// MAP
+// const users  = [
+//   {id: 1, name:'John'},
+//   {id: 2, name: 'Sara'},
+//   {id: 3, name: 'Karen'},
+//   {id: 4, name: 'Steve'}
+// ];
+
+// const ids = users.map(function(user){
+//   return user.id;
+// });
+
+// console.log(ids);
+
+
+// FOR IN LOOP
+const user = {
+  firstName: 'John', 
+  lastName: 'Doe',
+  age: 40
 }
 
-// console.log(greet());
-
-//Expressions 
-
-const square = function(x = 3) { //treating 3 as the default 
-    return x * x;
-};
-
-// console.log(square());
-
-//IIFEs - function declared and ran at the same time 
-// (function(){
-//     console.log('IIFE ran...');
-// })();
-
-// (function(name){
-//     console.log('Hello ' + name);
-// })('Brad');
-
-//Property Methods
-
-const todo = {
-    add: function(){
-        console.log('Add todo...');
-    },
-    edit: function(id){
-        console.log(`Edit todo ${id}`)
-    }
+for(let x in user){
+  console.log(`${x} : ${user[x]}`);
 }
-
-todo.delete = function(){
-    console.log('Delete todo')
-}
-
-todo.add();
-todo.edit(22);
-todo.delete();
